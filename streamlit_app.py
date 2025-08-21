e518935eff76880e0830a3dbde2fd4d207da2208
yana-b <yana.ballantyne@gmail.com>
2025-08-21 20:18:01 +0100

    Update streamlit_app.py
    v2 New section to display smoothiefroot nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie :cup_with_straw:")
@@ -54,3 +50,9 @@ if ingredients_list:
         
         # st.success('Your Smoothie is ordered, {name_on_order}!', icon="✅")
         st.success("Your Smoothie is ordered, {}!".format(name_on_order), icon="✅")
+
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
