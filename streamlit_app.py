c83a97c4f6cd047e77732d6636bc71bdc9f02ade
yana-b <yana.ballantyne@gmail.com>
2025-08-21 20:46:04 +0100

    Update streamlit_app.py
    panda import fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
-import pandas
+import pandas as pd
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
